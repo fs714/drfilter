587b82059b092e51fca065a6db918476ca4161f3
fs714 <fs714@163.com>
2015-07-13 00:53:52 +0800

    Refactor project structure

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import os
 
 path_list = sys.path
 
-src_path = os.path.dirname(os.path.abspath(__file__))
+src_path = os.path.dirname(os.path.abspath(__file__)) + "/drfilter"
 des_path = ""
 for path in path_list:
     if str(path).endswith('python2.7/dist-packages'):
